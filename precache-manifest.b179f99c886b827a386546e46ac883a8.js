041f973fe0f6ade7eef881adf036f8576f1b7a9a
Vo Kong Kiet <vokongkiet@gmail.com>
2020-05-20 10:33:11 +0700

    Updates

---
+++ precache-manifest.b179f99c886b827a386546e46ac883a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "849624e9fe6ffb3ba2482e9abbbd4aab",
+    "url": "/reactshop-deploy/index.html"
+  },
+  {
+    "revision": "cce6576a29272cc18d81",
+    "url": "/reactshop-deploy/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "fe3f4f79f92f8874b2b6",
+    "url": "/reactshop-deploy/static/css/main.fee873d0.chunk.css"
+  },
+  {
+    "revision": "cce6576a29272cc18d81",
+    "url": "/reactshop-deploy/static/js/2.8be6c699.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/reactshop-deploy/static/js/2.8be6c699.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe3f4f79f92f8874b2b6",
+    "url": "/reactshop-deploy/static/js/main.03f38422.chunk.js"
+  },
+  {
+    "revision": "ea82f49a119daec03f14",
+    "url": "/reactshop-deploy/static/js/runtime-main.8bd09f63.js"
+  }
+]);
